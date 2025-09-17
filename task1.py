b77ca1b9e8de9b439e3a2357946ffbed47c9da5c
Gosling121 <121379808+Gosling121@users.noreply.github.com>
2025-09-17 23:57:16 +0300

    calculate

--- task1.py
+++ task1.py
@@ -1,1 +1,15 @@
-print("hello world")
+a = int(input("Введите первое число "))
+b = int(input("Введите второе число "))
+c = str(input("Введите математическую операцию "))
+
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="/":
+    print(a/b)
+elif c=="*":
+    print(a*b)
+    
+
+

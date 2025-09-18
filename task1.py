77df176f7e02734904cfbb3d1ba3108877102107
Gosling121 <121379808+Gosling121@users.noreply.github.com>
2025-09-18 17:57:16 +0300

    task 1 complied

--- task1.py
+++ task1.py
@@ -1,15 +1,43 @@
-a = int(input("Введите первое число "))
-b = int(input("Введите второе число "))
-c = str(input("Введите математическую операцию "))
+history = {
+    "+":[],
+    "-":[],
+    "*":[],
+    "/":[]
+}
+while True:
+    try:
+        a = int(input("Введите первое число "))
+        b = int(input("Введите второе число "))
+        c = str(input("Введите математическую операцию "))
+        
+        if c=="+":
+            result = a + b
+            print(result)
+            history["+"].append(f"{a}+{b}={result}")
+        elif c=="-":
+            result = a - b
+            print(result)
+            history["-"].append(f"{a}-{b}={result}")
+        elif c=="/":
+            result = a / b
+            print(result)
+            history["/"].append(f"{a}/{b}={result}")
+        elif c=="*":
+            result = a * b
+            print(result)
+            history["*"].append(f"{a}*{b}={result}")
+        else:
+            print("такой операции нет")
+        
+        
+        for operation, operations_list in history.items():
+            print(f"{operation} {operations_list}")
+                
+    except ValueError:
+        print("Ошибка: введите корректные числа!")
+    except ZeroDivisionError:
+        print("Ошибка: деление на ноль!")
 
-if c=="+":
-    print(a+b)
-elif c=="-":
-    print(a-b)
-elif c=="/":
-    print(a/b)
-elif c=="*":
-    print(a*b)
     
 
 
